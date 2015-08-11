5cf17ba5f3aab1ca0a8feaca0ea3683dd82e8437
DongJialin <80927378@qq.com>
2015-08-11 22:29:40 +0800

    Add three Activitys Login Register Main page
    Signed-off-by: DongJialin <80927378@qq.com>

--- java/com/momentpeace/silent/LoginActivity.java
+++ java/com/momentpeace/silent/LoginActivity.java
@@ -1,17 +1,53 @@
 package com.momentpeace.silent;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 
-public class LoginActivity extends ActionBarActivity {
+public class LoginActivity extends Activity {
+
+    EditText mUserName;
+    EditText mPassword;
+    Button mRegister;
+    Button mLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mUserName = (EditText)findViewById(R.id.user_name_input);
+        mPassword = (EditText)findViewById(R.id.password_input);
+        mRegister = (Button)findViewById(R.id.button_register);
+        mLogin = (Button)findViewById(R.id.button_login);
+
+        mRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                //String user_name = (String)mUserName.getText();
+                startActivity(intent);
+            }
+        });
+
+        mLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                String user_name = mUserName.getText().toString();
+                String password = mPassword.getText().toString();
+                Bundle bundle = new Bundle();
+                bundle.putString("user.name",user_name);
+                bundle.putString("password",password);
+                intent.putExtras(bundle);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
