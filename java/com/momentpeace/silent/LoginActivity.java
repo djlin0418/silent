8f218a5bab75f4081661212c4bd8f02897b9c662
DongJialin <80927378@qq.com>
2015-08-13 21:21:57 +0800

    Add hanler to handle Login events
    Just a beginning of processing flow, need to be perfection.
    
    Signed-off-by: DongJialin <80927378@qq.com>

--- java/com/momentpeace/silent/LoginActivity.java
+++ java/com/momentpeace/silent/LoginActivity.java
@@ -3,6 +3,10 @@ package com.momentpeace.silent;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -12,10 +16,13 @@ import android.widget.EditText;
 
 public class LoginActivity extends Activity {
 
-    EditText mUserName;
-    EditText mPassword;
-    Button mRegister;
-    Button mLogin;
+    private static final int EVENT_VERIFY= 0;
+    private static final int EVENT_LOGIN = 1;
+    private EditText mUserName;
+    private EditText mPassword;
+    private Button mRegister;
+    private Button mLogin;
+    private InternalHandler mInternalHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +32,8 @@ public class LoginActivity extends Activity {
         mPassword = (EditText)findViewById(R.id.password_input);
         mRegister = (Button)findViewById(R.id.button_register);
         mLogin = (Button)findViewById(R.id.button_login);
+        mInternalHandler = new InternalHandler();
+
 
         mRegister.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,18 +47,36 @@ public class LoginActivity extends Activity {
         mLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+//                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                 String user_name = mUserName.getText().toString();
                 String password = mPassword.getText().toString();
                 Bundle bundle = new Bundle();
                 bundle.putString("user.name",user_name);
-                bundle.putString("password",password);
-                intent.putExtras(bundle);
-                startActivity(intent);
+                bundle.putString("password", password);
+//                intent.putExtras(bundle);
+                Message message = mInternalHandler.obtainMessage(EVENT_VERIFY, bundle);
+                mInternalHandler.sendMessage(message);
+//                startActivity(intent);
             }
         });
     }
 
+    private class InternalHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case EVENT_VERIFY:
+                    Bundle bundle = (Bundle)msg.obj;
+                    String user_name = bundle.getString("user.name");
+                    String password = bundle.getString("password");
+                case EVENT_LOGIN:
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    LoginActivity.this.startActivity(intent);
+                default:
+                    super.handleMessage(msg);
+            }
+        }
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
